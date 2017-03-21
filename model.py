7ffdd764892fbbd639a87cb9dd790f19161eed71
Prashant Gaikwad <pgaikwad@nvidia.com>
2017-03-21 20:20:26 +0530

    update angle range

--- model.py
+++ model.py
@@ -106,10 +106,10 @@ zero_count = 0
 nonzero_count = 0
 for sample in train_samples_temp:
 	test = randint(0,9)
-	if test == 3 or float(sample[3]) > 0.2 or float(sample[3]) < -0.2:
+	if test == 3 or float(sample[3]) > 0.1 or float(sample[3]) < -0.1:
 		train_samples.append(sample)
 		total_count = total_count + 1
-		if float(sample[3]) > 0.2 or float(sample[3]) < -0.2:
+		if float(sample[3]) > 0.1 or float(sample[3]) < -0.1:
 			total_count = total_count + 3
 			nonzero_count = nonzero_count + 4
 			train_samples.append(sample)
@@ -127,10 +127,10 @@ zero_count = 0
 nonzero_count = 0
 for sample in validation_samples_temp:
 	test = randint(0,9)
-	if test == 3 or float(sample[3]) > 0.2 or float(sample[3]) < -0.2:
+	if test == 3 or float(sample[3]) > 0.1 or float(sample[3]) < -0.1:
 		validation_samples.append(sample)
 		total_count = total_count + 1
-		if (float(sample[3]) > 0.2 or float(sample[3]) < -0.2):
+		if (float(sample[3]) > 0.1 or float(sample[3]) < -0.1):
 			total_count = total_count + 3
 			nonzero_count = nonzero_count + 4
 			validation_samples.append(sample)
@@ -247,5 +247,5 @@ if __name__ == '__main__':
 	model = get_model()
 	model.fit_generator(train_generator, steps_per_epoch=(len(train_samples)+15)/16,\
 			validation_data=validation_generator, nb_val_samples=len(validation_samples),\
-			nb_epoch=7)
+			nb_epoch=10)
 	model.save('model.h5')
